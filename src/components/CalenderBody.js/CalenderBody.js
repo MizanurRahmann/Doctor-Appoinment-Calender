f54b3352d6e2cbb0e62592ae6e87f666f71ba000
MizanurRahmann <mizanur.rahman03032@gmail.com>
2022-07-29 00:17:15 +0600

    ADD: date grid.

--- src/components/CalenderBody.js/CalenderBody.js
+++ src/components/CalenderBody.js/CalenderBody.js
@@ -1,14 +1,34 @@
 import React, { useEffect } from "react";
+import styled from "styled-components";
 
-function CalenderBody({currenMonth}) {
-  
-    useEffect(() => {
-    console.log(currenMonth);
-  });
+function CalenderBody({ currenMonth }) {
+  const Box = styled.div`
+    width: 14.1%;
+    height: 16vh;
+    border: 1px solid #ccc;
+    padding: 20px;
+    transition: 0.3s;
+    cursor: pointer;
+    &:hover{
+      background: #f7f7f7;
+    }
+  `;
 
-  return <div>
-    OK
-  </div>;
+
+  return (
+    <div className="p-5 d-flex flex-wrap">
+      {currenMonth &&
+        currenMonth.map((row) => (
+          <>
+            {row.map((col) => (
+              <Box key={Math.random()}>
+                <p>{col.$D}</p>
+              </Box>
+            ))}
+          </>
+        ))}
+    </div>
+  );
 }
 
 export default CalenderBody;
